971dcf525b864f86933461e63a64741f5fe86b0d
Bård Økstra <baard.oekstra93@gmail.com>
2024-09-25 15:23:28 +0200

    Fått inn filene
    Må endre det i with-løkkene til å sortere info i stedet for å lese ut linjene.

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -1,4 +1,14 @@
-import temperatur_trykk_met_samme_rune_time_datasett
-import trykk_og_temperaturlogg_rune_time
+import csv
+
+with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file1:
+    reader1 = csv.reader(file1)
+    for row in reader1:
+        print(row)
+
+with open('trykk_og_temperaturlogg_rune_time.csv', mode='r') as file2:
+    reader2 = csv.reader(file2)
+    for row in reader2:
+        print(row)
+
 
 stasjon = list()
