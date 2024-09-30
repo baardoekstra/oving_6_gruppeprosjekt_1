3cba74174f3da4630e7b232d8144e78150696811
Bård Økstra <baard.oekstra93@gmail.com>
2024-09-30 11:35:44 +0200

    asdasd
    asd

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -1,22 +1,17 @@
 import csv
 
 with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file1:
-    reader1 = csv.reader(file1)
+    reader1 = csv.reader(file1, delimiter=";")
+    temperatur = list()
+    lufttrykk = list()
+    tidspunkt1 = list()
     for row in reader1:
-        print(row)
+        tidspunkt1.append(row[2])
+        temperatur.append(row[3])
+        lufttrykk.append(row[4])
+
+print(temperatur)
 
 with open('trykk_og_temperaturlogg_rune_time.csv', mode='r') as file2:
     reader2 = csv.reader(file2)
     for row in reader2:
-        print(row)
-
-
-stasjon = list()
-
-x = 45
-sdgfs
-
-Hei på DeprecationWarning
-
-for i in range(10):
-    dsf
