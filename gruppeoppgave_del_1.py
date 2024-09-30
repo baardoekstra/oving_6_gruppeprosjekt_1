69e0d1677e9a5c8d7b97282f522318a160e2acad
Bård Økstra <baard.oekstra93@gmail.com>
2024-09-30 11:52:13 +0200

    La til 1
    La til 1

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -3,15 +3,15 @@ import matplotlib.pyplot as plt
 
 with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file1:
     reader1 = csv.reader(file1, delimiter=";")
-    temperatur = list()
-    lufttrykk = list()
+    temperatur1 = list()
+    lufttrykk1 = list()
     tidspunkt1 = list()
     for row in reader1:
         tidspunkt1.append(row[2])
-        temperatur.append(row[3])
-        lufttrykk.append(row[4])
+        temperatur1.append(row[3])
+        lufttrykk1.append(row[4])
 
-print(temperatur)
+print(tidspunkt1, temperatur1, lufttrykk1)
 
 with open('trykk_og_temperaturlogg_rune_time.csv', mode='r') as file2:
     reader2 = csv.reader(file2, delimiter=";") #Delimiter gir informasjon til programmet om at elementene skal "deles" ved hver semikolon.
