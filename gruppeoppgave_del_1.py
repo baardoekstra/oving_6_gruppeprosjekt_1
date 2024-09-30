2101d7a1cf61c78ab442ba1a6ef8a4aaf55ef10b
Akalvo <237947@uis.no>
2024-09-30 11:48:26 +0200

    Update gruppeoppgave_del_1.py

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -1,33 +1,28 @@
 import csv
 
-with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file1:
-    reader1 = csv.reader(file1, delimiter=";")
-    temperatur = list()
-    lufttrykk = list()
-    tidspunkt1 = list()
-    for row in reader1:
-        tidspunkt1.append(row[2])
-        temperatur.append(row[3])
-        lufttrykk.append(row[4])
+#with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file1:
+ #   reader1 = csv.reader(file1, delimiter=";")
+  #  temperatur = list()
+   # lufttrykk = list()
+    #tidspunkt1 = list()
+    #for row in reader1:
+    #    tidspunkt1.append(row[2])
+    #    temperatur.append(row[3])
+    #    lufttrykk.append(row[4])
 
-print(temperatur)
+#print(temperatur)
 
 with open('trykk_og_temperaturlogg_rune_time.csv', mode='r') as file2:
-<<<<<<< HEAD
     reader2 = csv.reader(file2, delimiter=";") #Delimiter gir informasjon til programmet om at elementene skal "deles" ved hver semikolon.
-    reader2 = list()
     temperatur2 = list()
     tidspunkt2 = list()
-    for row in reader2:
-        temperatur2.append(row[1])
-        tidspunkt2.append(row[0])
+    for row2 in reader2:
+        temperatur2.append(row2[-1])
+        tidspunkt2.append(row2[0])
         #print(
-    print(temperatur2, tidspunkt2)
+    print(tidspunkt2)
+    print(temperatur2)
 
 
 
 
-=======
-    reader2 = csv.reader(file2)
-    for row in reader2:
->>>>>>> 3cba74174f3da4630e7b232d8144e78150696811
