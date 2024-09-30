2551939302ac8d61e8cafab020c1d561396afb4e
Akalvo <237947@uis.no>
2024-09-30 11:36:54 +0200

    Update gruppeoppgave_del_1.py

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -6,17 +6,16 @@ with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file
         print(row)
 
 with open('trykk_og_temperaturlogg_rune_time.csv', mode='r') as file2:
-    reader2 = csv.reader(file2)
+    reader2 = csv.reader(file2, delimiter=";") #Delimiter gir informasjon til programmet om at elementene skal "deles" ved hver semikolon.
+    reader2 = list()
+    temperatur2 = list()
+    tidspunkt2 = list()
     for row in reader2:
-        print(row)
+        temperatur2.append(row[1])
+        tidspunkt2.append(row[0])
+        #print(
+    print(temperatur2, tidspunkt2)
 
 
-stasjon = list()
 
-x = 45
-sdgfs
 
-Hei på DeprecationWarning
-
-for i in range(10):
-    dsf
