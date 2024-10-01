167dbc310c1b61900b07f356b3b750fc950f2541
Bård Økstra <baard.oekstra93@gmail.com>
2024-10-01 13:49:47 +0200

    Lagd masse kaos
    Gjør så godt eg kan

--- gruppeoppgave_del_1.py
+++ gruppeoppgave_del_1.py
@@ -7,10 +7,13 @@ with open('temperatur_trykk_met_samme_rune_time_datasett.csv', mode='r') as file
     temperatur1 = list()
     lufttrykk1 = list()
     tidspunkt1 = list()
+
     for row in reader1:
         tidspunkt1.append(row[2])
-        temperatur1.append(row[3])
-        lufttrykk1.append(row[4])
+        temperatur1.append(row[3].replace(',', '.'))
+        temperatur1_verdier = float(temperatur1[1:])
+        lufttrykk1.append(row[4].replace(',', '.'))
+        lufttrykk1_verdier = float(lufttrykk1[1:])
 
 
 
@@ -19,15 +22,18 @@ with open('trykk_og_temperaturlogg_rune_time.csv', mode='r') as file2:
     temperatur2 = list()
     tidspunkt2 = list()
     for row2 in reader2:
-        temperatur2.append(row2[-1])
+        temperatur2.append(row2[-1].replace(',', '.'))
+        temperatur2_verdier = float(temperatur2[1:])
         tidspunkt2.append(row2[0])
 
-for i in range(len(tidspunkt2 - 1)):
-    tidspunkt2[i] = datetime.strptime(tidspunkt2[i], '%m-%d-%Y').strftime('%d.%m.%Y')
+for i in range(len(tidspunkt1) - 1):
+    tidspunkt1[i+1] = datetime.strptime(tidspunkt1[i+1])
+for i in range(len(tidspunkt2) - 1):
+    tidspunkt2[i+1] = datetime.strptime(tidspunkt2[i+1], '%m.%d.%Y %H:%M').strftime('%d.%m.%Y %H:%M')
 
 plt.figure(figsize=(12, 8))
-plt.plot(tidspunkt1[1:], temperatur1[1:], label=temperatur1[0], color="red", linewidth=4)
-plt.plot(tidspunkt2[1:], temperatur2[1:], label=temperatur2[0], color= "blue", linewidth=4)
+plt.plot(tidspunkt1[1:], temperatur1_verdier[1:], label="Temperatur1", color="red", linewidth=4)
+plt.plot(tidspunkt2[1:], temperatur2_verdier[1:], label="Temperatur2", color= "blue", linewidth=4)
 plt.xlabel("Tidspunkter")
 plt.ylabel("Verdier")
 
